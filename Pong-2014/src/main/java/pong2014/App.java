b2d1cc23cd503cc3650d8e06eb5f0e351f65a809
Jone <joonas.laakkonen@gmail.com>
2014-01-16 10:32:38 +0200

    Initial commit for new project

---
+++ Pong-2014/src/main/java/pong2014/App.java
@@ -0,0 +1,13 @@
+package pong2014;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
